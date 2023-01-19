602cf370753660bc0e375e4e143115035f534881
Devashish Das <97696912+DevashishDas3@users.noreply.github.com>
2023-01-18 19:41:48 -0500

    ez

---
+++ adBlocker/background.js
@@ -0,0 +1,26 @@
+setInterval(() => {
+    let skipButtion = document.getElementsByClassName("ytp-ad-skip-button");
+    if (skipButtion != undefined && skipButtion.length > 0) {
+        console.log("skipped ad");
+        skipButtion[0].click();
+    }
+}, 3000);
+
+const defaultFilters = [
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+]
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return { cancel: true }},
+    { urls: defaultFilters },
+    ["blocking"]
+)
